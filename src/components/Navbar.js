6d4a94c8c9b126f93359d14529733c7b36d5d5cb
Deep Prajapati <dprajap2@gmail.com>
2021-03-14 15:22:56 -0400

    Changes font and creates a music player component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,109 +1,127 @@
 import React from "react";
+import ReactAudioPlayer from "react-audio-player";
+
 import { getData } from "../helper-function/getYelpData";
 
-export default class Navbar extends React.Component{
-  constructor(props){
+export default class Navbar extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
-      cafes:[],
-      order:""
-    }
-    this.onBack = this.onBack.bind(this)
-    this.onOrder = this.onOrder.bind(this)
+      cafes: [],
+      order: "",
+    };
+    this.onBack = this.onBack.bind(this);
+    this.onOrder = this.onOrder.bind(this);
   }
 
   async onOrder(event, location, item) {
     try {
       event.preventDefault();
-      const cafeData= await getData(location, item);
+      const cafeData = await getData(location, item);
       this.setState({
-        order: true ,
-       cafes:cafeData.data.businesses
-     })
-
+        order: true,
+        cafes: cafeData.data.businesses,
+      });
     } catch (error) {
-      console.log('Error in getting yelp data', error)
+      console.log("Error in getting yelp data", error);
     }
-
   }
 
-
-  onBack(event){
-    event.preventDefault()
+  onBack(event) {
+    event.preventDefault();
     this.setState({
-      order:false
-    })
-    console.log('in the back', this.state)
+      order: false,
+    });
+    console.log("in the back", this.state);
   }
 
-  render (){
-  const videos = this.props.videos;
-  const items = this.props.items
+  render() {
+    const videos = this.props.videos;
+    const items = this.props.items;
 
-  return (
-    <div id="container">
-      <h1>Cafe Code Brew</h1>
-      <div className="navbar-container">
-        <div className="categorys">
-          <h4> Pick your cafe </h4>
-          {videos.map((video) => {
-            return (
-              <button
-                onClick={(event) => this.props.getVideo(event, video)}
-                type="submit"
-                key={video.title}
-              >
-                {video.title}
-              </button>
-            );
-          })}
-        </div>
-        <div className="order">
-        {!this.state.order ?
-        <div>
-          <h4>Find</h4>
-          <div className="foods">
-            {items && items.map((item) => {
+    return (
+      <div id="container">
+        <h1>Cafe Code Brew</h1>
+        <div className="navbar-container">
+          <div className="categorys">
+            <h4> Pick your cafe </h4>
+            {videos.map((video) => {
               return (
-                <span
-                  className="food-images"
-                  key={item.name}
-                  onClick={(event) => this.onOrder(event,'nyc', item.name)}
+                <button
+                  onClick={(event) => this.props.getVideo(event, video)}
+                  type="submit"
+                  key={video.title}
                 >
-                  <img
-                    src={item.imageUrl}
-                    alt="coffee cup"
-                    width="50"
-                    height="50"
-                  />
-                </span>
+                  {video.title}
+                </button>
               );
             })}
           </div>
-          </div> :
-          <div>
-            <button onClick={(event)=>{this.onBack(event)}}>Find Somthing Else</button>
-            {this.state.cafes[0].name}
-            <br/>
-            {this.state.cafes[0].location.display_address}
-            <br/>
-            {this.state.cafes[1].name}
-            <br/>
-            {this.state.cafes[1].location.display_address}
-          </div>}
+          <div className="music-player">
+          <h4> Pick your music </h4>
+            <ReactAudioPlayer src="http://node-24.zeno.fm/60ef4p33vxquv?rj-ttl=5&rj-tok=AAABcfD5im8AKkQwULjleajGRg" autoPlay volume/>
+          </div>
+          <div className="order">
+            {!this.state.order ? (
+              <div>
+                <h4>Find</h4>
+                <div className="foods">
+                  {items &&
+                    items.map((item) => {
+                      return (
+                        <span
+                          className="food-images"
+                          key={item.name}
+                          onClick={(event) =>
+                            this.onOrder(event, "nyc", item.name)
+                          }
+                        >
+                          <img
+                            src={item.imageUrl}
+                            alt="food items "
+                            width="50"
+                            height="50"
+                          />
+                        </span>
+                      );
+                    })}
+                </div>
+              </div>
+            ) : (
+              <div>
+                <button
+                  onClick={(event) => {
+                    this.onBack(event);
+                  }}
+                >
+                  Find Somthing Else
+                </button>
+
+                <div>
+                  {this.state.cafes[0].name}
+                  <br />
+                  {this.state.cafes[0].location.display_address}
+                  <br />
+                  {this.state.cafes[1].name}
+                  <br />
+                  {this.state.cafes[1].location.display_address}
+                </div>
+              </div>
+            )}
+          </div>
           <div className="coffee-cup">
-            <img
-              src="http://clipart-library.com/images/di48x8LAT.jpg"
-              alt="coffee cup"
-              width="50"
-              height="50"
-            />
+            <h4>Invite A Friend</h4>
+            <span className="cup-image">
+              <img
+                src="http://clipart-library.com/images/di48x8LAT.jpg"
+                alt="coffee cup"
+                width="50"
+                height="50"
+              />
+            </span>
           </div>
         </div>
       </div>
-    </div>
-  )
+    );
+  }
 }
-}
-
-
---
+++ src/data/music-data.js
@@ -0,0 +1,23 @@
+
+const data = [{
+  title:'Outdoor Cafe',
+  url:"http://node-24.zeno.fm/60ef4p33vxquv?rj-ttl=5&rj-tok=AAABcfD5im8AKkQwULjleajGRg"
+},
+{
+  title:'Beach Cafe',
+  url:"https://www.youtube.com/watch?v=6qsPKvt-w84&ab_channel=SeafoamZen"
+},
+{
+  title:'Tokyo Cafe',
+  url:"https://www.youtube.com/watch?v=qMHbh6XmgwM&t=103"
+},
+{
+  title: 'Animated Rainy Cafe',
+  url: 'https://www.youtube.com/watch?v=c0_ejQQcrwI&ab_channel=CalmedByNature'
+
+}
+
+]
+
+export default data;
+
